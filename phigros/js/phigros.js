97ed5ab0844ff2a2181ed554d1da9e2c3b76a37e
KCFindstr <changyuz@usc.edu>
2019-08-18 02:38:29 -0700

    Phigros: Update images and javascript to set section height.

--- phigros/js/phigros.js
+++ phigros/js/phigros.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
 	$('#fullpage').fullpage({
 		scrollOverflow: true
 	});
+	/* Make sure it gets full height
+	 * Some weird things happen on mobile devices when I use 100vh
+	 */
+	$('.section-5a').height(window.innerHeight);
 });
