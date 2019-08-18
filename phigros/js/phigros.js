92744e55d6d262a094c8d0ffe4374beec68fe097
KCFindstr <changyuz@usc.edu>
2019-08-18 05:59:27 -0700

    Update styling

--- phigros/js/phigros.js
+++ phigros/js/phigros.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
 	$('#fullpage').fullpage({
-		scrollOverflow: true
+		scrollOverflow: true,
+		paddingTop: '50px'
 	});
 	/* Make sure it gets full height
 	 * Some weird things happen on mobile devices when I use 100vh
 	 */
-	$('.section-5a').height(window.innerHeight);
+	// $('.section-5a').height(window.innerHeight);
 });
